7d481d8a3d37d81c2679647ebb0328190eeb1f22
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2018-06-21 17:16:40 +0700

    .

--- perwita/app/d_pekerja.php
+++ perwita/app/d_pekerja.php
@@ -16,7 +16,7 @@ class d_pekerja extends Model
     const CREATED_AT = 'p_insert';
 
 
-    protected $fillable = ["p_id","p_jabatan","p_jabatan_lamaran","p_nip","p_nip_mitra","p_ktp","p_name","p_sex","p_birthplace","p_birthdate","p_hp","p_telp","p_status","p_many_kids","p_religion","p_address","p_rt_rw","p_kel","p_kecamatan","p_city","p_address_now","p_rt_rw_now","p_kel_now","p_kecamatan_now","p_city_now","p_name_family","p_address_family","p_telp_family","p_hp_family","p_hubungan_family","p_wife_name","p_wife_birth", "p_wife_birthplace","p_dad_name","p_dad_job","p_mom_name","p_mom_job","p_job_now","p_weight","p_height","p_seragam_size","p_celana_size","p_sepatu_size","p_kpk","p_bu","p_ktp_expired","p_ktp_seumurhidup","p_education","p_kpj_no","p_state","p_note","p_workdate","p_img","p_insert","p_update","p_date_approval"];
+    protected $fillable = ["p_id","p_jabatan","p_jabatan_lamaran","p_nip","p_nip_mitra","p_ktp","p_name","p_sex","p_birthplace","p_birthdate","p_hp","p_telp","p_status","p_many_kids","p_religion","p_address","p_rt_rw","p_kel","p_kecamatan","p_city","p_address_now","p_rt_rw_now","p_kel_now","p_kecamatan_now","p_city_now","p_name_family","p_address_family","p_telp_family","p_hp_family","p_hubungan_family","p_wife_name","p_wife_birth", "p_wife_birthplace","p_dad_name","p_dad_job","p_mom_name","p_mom_job","p_job_now","p_weight","p_height","p_seragam_size","p_celana_size","p_sepatu_size","p_kpk","p_bu","p_ktp_expired","p_ktp_seumurhidup","p_education","p_kpj_no","p_state","p_note","p_workdate","p_img","p_insert","p_update","p_date_approval", "p_status_approval"];
 
     protected $dates = ['p_tgl_lahir'];
 
