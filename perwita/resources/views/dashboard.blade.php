9c8545171328e8af470d8acb96099cefd6a6d7e4
Muhammad Ferdy Cahya Pratama <ferdyp73@gmail.com>
2018-07-12 13:53:08 +0700

    .

--- perwita/resources/views/dashboard.blade.php
+++ perwita/resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('extra_styles')
-  
+
   <style>
     .popover-navigation [data-role="next"] { display: none; }
     .popover-navigation [data-role="end"] { display: none; }
@@ -13,128 +13,76 @@
 
 @section('content')
 
-  
+
         <div class="row border-bottom  dashboard-header">
-       
-            
-    <div class="row">
-      <div class="col-lg-4 col-xs-12">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-          <div class="inner">
-            <h3>
-             <sup style="font-size: 20px">Rp 27.000.0000,00</sup>                         
-                </h3>
-            <p>Total Penjualan</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-            <div style="height:30px;">                
-            </div>
-        </div>
-      </div>
-        
-        <div class="col-lg-4 col-xs-12">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>
-             <sup style="font-size: 20px">Rp 27.000.0000,00</sup>                         
-                </h3>
-            <p>Total Penjualan</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-            <div style="height:30px;">                
-            </div>
-        </div>
-      </div>
-        
-        <div class="col-lg-4 col-xs-12">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-          <div class="inner">
-            <h3>
-             <sup style="font-size: 20px">Rp 27.000.0000,00</sup>                         
-                </h3>
-            <p>Total Penjualan</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-            <div style="height:30px;">                
-            </div>
-        </div>
-      </div>
-        
-    </div>
-    <div class="row">
-      <div class="col-lg-4 col-xs-12">
-        <!-- small box -->
-        <div class="small-box bg-aqua">
-          <div class="inner">
-            <h3>
-             <sup style="font-size: 20px">Rp 27.000.0000,00</sup>                         
-                </h3>
-            <p>Total Penjualan</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-            <div style="height:30px;">                
-            </div>
-        </div>
-      </div>
-        
-        <div class="col-lg-4 col-xs-12">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>
-             <sup style="font-size: 20px">Rp 27.000.0000,00</sup>                         
-                </h3>
-            <p>Total Penjualan</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-            <div style="height:30px;">                
-            </div>
-        </div>
-      </div>
-        
-        <div class="col-lg-4 col-xs-12">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-          <div class="inner">
-            <h3>
-             <sup style="font-size: 20px">Rp 27.000.0000,00</sup>                         
-                </h3>
-            <p>Total Penjualan</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-bag"></i>
-          </div>
-            <div style="height:30px;">                
-            </div>
-        </div>
-      </div>
-        
-    </div>
-            
-            
-            
+
+
+          <div class="row">
+                          <div class="col-lg-3">
+                              <div class="ibox float-e-margins">
+                                  <div class="ibox-title">
+                                      <span class="label label-success pull-right">Monthly</span>
+                                      <h5>Income</h5>
+                                  </div>
+                                  <div class="ibox-content">
+                                      <h1 class="no-margins">40 886,200</h1>
+                                      <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                                      <small>Total income</small>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-lg-3">
+                              <div class="ibox float-e-margins">
+                                  <div class="ibox-title">
+                                      <span class="label label-info pull-right">Annual</span>
+                                      <h5>Orders</h5>
+                                  </div>
+                                  <div class="ibox-content">
+                                      <h1 class="no-margins">275,800</h1>
+                                      <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
+                                      <small>New orders</small>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-lg-3">
+                              <div class="ibox float-e-margins">
+                                  <div class="ibox-title">
+                                      <span class="label label-primary pull-right">Today</span>
+                                      <h5>Vistits</h5>
+                                  </div>
+                                  <div class="ibox-content">
+                                      <h1 class="no-margins">106,120</h1>
+                                      <div class="stat-percent font-bold text-navy">44% <i class="fa fa-level-up"></i></div>
+                                      <small>New visits</small>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-lg-3">
+                              <div class="ibox float-e-margins">
+                                  <div class="ibox-title">
+                                      <span class="label label-danger pull-right">Low value</span>
+                                      <h5>User activity</h5>
+                                  </div>
+                                  <div class="ibox-content">
+                                      <h1 class="no-margins">80,600</h1>
+                                      <div class="stat-percent font-bold text-danger">38% <i class="fa fa-level-down"></i></div>
+                                      <small>In first month</small>
+                                  </div>
+                              </div>
+                  </div>
+              </div>
+
+
+
         </div>
 
-    
+
 
 @endsection
 
 @section('extra_scripts')
   <script type="text/javascript">
-    
-   
+
+
   </script>
 @endsection
