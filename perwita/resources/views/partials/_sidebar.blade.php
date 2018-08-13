35a455569698c406ea64249cc45434056c44c26f
ilhamroliz <ilham.rolis@gmail.com>
2018-08-13 14:36:16 +0700

    roliz

--- perwita/resources/views/partials/_sidebar.blade.php
+++ perwita/resources/views/partials/_sidebar.blade.php
@@ -127,6 +127,7 @@
                     <li class="{{ Request::is('manajemen-seragam/pembelian') ? 'active' : ''
                                 || Request::is('manajemen-seragam/gunakan-rencana-pembelian') ? 'active' : '' 
                                 || Request::is('manajemen-seragam/edit') ? 'active' : '' 
+                                || Request::is('manajemen-seragam/tambah') ? 'active' : '' 
                                 }} sidebar master-akun">
                         <a href="{{ url('manajemen-seragam/pembelian') }}">
                             <i class="" aria-hidden="true"></i><span class="nav-label">Pembelian Seragam</span>
